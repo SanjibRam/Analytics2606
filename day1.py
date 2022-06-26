ff60756424e5640c2ba14448168ee7ba00cef394
vikaskhullar <vikas.khullar@gmail.com>
2022-06-26 19:38:38 +0530

    update

---
+++ day1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 25 20:35:43 2022
+
+@author: vikas
+"""
+
+a=10
+b=20
+c=a+b
+print(c)
+
+l1 = range(1,11)
+l2 = range(10,20)
+
+import matplotlib.pyplot as plt
+plt.plot(l1,l2)
